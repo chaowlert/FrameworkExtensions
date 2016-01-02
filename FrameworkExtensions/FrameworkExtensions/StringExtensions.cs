85d8126d76d6fc54d28acf0e15bc2beda128c527
chaowlert <chaowlert@gmail.com>
2016-01-02 17:26:52 +0700

    add nuspec

--- FrameworkExtensions/FrameworkExtensions/StringExtensions.cs
+++ FrameworkExtensions/FrameworkExtensions/StringExtensions.cs
@@ -9,23 +9,6 @@ public static class StringExtensions
         return string.Join(separator, values);
     }
 
-    public static string Utf8Substring(this string text, int byteLimit)
-    {
-        int byteCount = 0;
-        char[] buffer = new char[1];
-        for (int i = 0; i < text.Length; i++)
-        {
-            buffer[0] = text[i];
-            byteCount += Encoding.UTF8.GetByteCount(buffer);
-            if (byteCount > byteLimit)
-            {
-                // Couldn't add this character. Return its index
-                return text.Substring(0, i);
-            }
-        }
-        return text;
-    }
-
     public static string RemoveSign(this string text)
     {
         return Regex.Replace(text, @"\W+", "");
